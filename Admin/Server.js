50279fe6a61eaf44f84a3d27c7085d3edbfde2d1
sourav.iss <sourav.iss2015@gmail.com>
2019-02-15 08:25:41 +0530

    Add AdminUI Project under Admin folder

--- Admin/Server.js
+++ Admin/Server.js
@@ -1,1 +1,53 @@
-console.log("starting on Way");
+const express=require('express');
+const bodyparser=require('body-parser');
+const cors=require('cors');
+const app=express();
+
+
+app.listen(8100,()=>{
+    console.log('Server Started!.. in this port');
+})
+
+app.use(bodyparser.json()); 
+
+var corsOptions={
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+};
+
+app.use(cors(corsOptions));
+
+
+
+//get Category Route
+app.route('/api/category').get((req,res)=>{
+    res.send({
+        cats:[{name:'category1'},{name:'category2'}]
+    });
+});
+//get catgory Route with dynamic string
+app.route('/api/cats/:name').get((req, res) => {
+    const requestedCatName = req.params['name'];
+    res.send({ name: requestedCatName });
+  });
+
+  //create a New category
+app.route('/api/category').post((req,res)=>{
+res.send(201,req.body);
+});
+//update category by its name
+app.route('/api/category/:name').put((req, res) => {
+    res.send(200, req.body);
+  });
+//delete category by its name
+  app.route('/api/category/:name').delete((req, res) => {
+    res.sendStatus(204);
+  });
+
+
+
+
+
+
